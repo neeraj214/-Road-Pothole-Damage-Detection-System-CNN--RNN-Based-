20480535780494602d6fc6025779cde13ef06e4e
Neeraj Negi <neerajnegi108333@gmail.com>
2026-03-20 00:09:21 +0530

    Pass original file to ResultCard for canvas overlay

--- frontend/src/pages/Detect.jsx
+++ frontend/src/pages/Detect.jsx
@@ -79,7 +79,7 @@ function Detect() {
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
             >
-              <ResultCard result={result} />
+              <ResultCard result={result} file={file} />
             </motion.div>
           )}
 
